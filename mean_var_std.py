4fa464b1fd12d19a2692aa6f731bff8883fcb876
lavishsaini01 <151519817+lavishsaini01@users.noreply.github.com>
2024-05-10 16:09:11 +0530

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 def calculate(list):
+    arr = np.array(list)
+    arr = arr.reshape(3,3)
 
+    mean =np.mean(arr, axis=0),np.mean(arr, axis=1),np.mean(arr)
+    var = np.var(arr, axis=0),np.var(arr, axis=1),np.var(arr)
+    std = np.std(arr, axis=0),np.std(arr, axis=1),np.std(arr)
+    max = np.max(arr, axis=0),np.max(arr, axis=1),np.max(arr)
+    max = np.min(arr, axis=0),np.min(arr, axis=1),np.min(arr)
+    sum = np.sum(arr, axis=0),np.sum(arr, axis=1),np.sum(arr)
 
-
-
-    return calculations
+    calculations = {"mean":mean,
+                   "variance": var,
+                   'standard deviation':std,
+                   'max': max,
+                   'min':min,
+                   'sum':sum}
+ return calculations
